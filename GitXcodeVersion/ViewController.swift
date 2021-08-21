fabb623175f61e238bf47a585082ce5604f9252e
andromegasoftware <kadiryapar16@gmail.com>
2021-08-21 10:27:39 +0300

    Initial Commit

---
+++ GitXcodeVersion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitXcodeVersion
+//
+//  Created by kadir yapar on 21.8.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
